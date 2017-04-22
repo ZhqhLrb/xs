ba8866974b949d0eaafe6b9f40b2daa2aee4a220
lrb2333 <591392447@qq.com>
2017-04-23 00:05:26 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function init () {
+    var cont = document.querySelector('.container');
+
+    // cont.style.height = window.innerHeight + 'px';
+
+    if (window.innerHeight / window.dpr >= 500) {
+        cont.style.height = window.innerHeight + 'px';
+    }
+}
+
+init();
