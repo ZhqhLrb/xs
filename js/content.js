c7eb75debced1fe81e06fcbb498e40663512e206
lrb2333 <591392447@qq.com>
2017-04-24 11:12:44 +0800

    fix

--- js/content.js
+++ js/content.js
@@ -7,10 +7,16 @@ window.onload = function () {
 
 function init() {
     var cont = document.querySelector('.container');
+    var alertBox = document.querySelector('.alert');
+    var btn = document.querySelector('.alert-confirm');
 
     if (window.innerHeight / window.dpr >= 500) {
         cont.style.height = window.innerHeight + 'px';
-    }   
+    }
+
+    btn.addEventListener('click', function () {
+        alertBox.style.display = 'none';
+    })
 }
 
 init();
--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 function init () {
     var cont = document.querySelector('.container');
+    var form = document.querySelector('.form');
+    var nameInput = document.querySelector('.names');
+    var btn = document.querySelector('.btn');
 
+    var alertBox = document.querySelector('.alert');
+    var btn = document.querySelector('.alert-confirm');
     // cont.style.height = window.innerHeight + 'px';
 
     if (window.innerHeight / window.dpr >= 500) {
         cont.style.height = window.innerHeight + 'px';
     }
+
+    form.onsubmit = function () {
+        var name = nameInput.value;
+        if (name.length > 5 || name.length <= 0) {
+            alertBox.style.display = 'block';
+            return false;
+        }
+    }
+
+    btn.addEventListener('click', function () {
+        alertBox.style.display = 'none';
+    })
 }
 
 init();
